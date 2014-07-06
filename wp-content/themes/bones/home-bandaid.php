6003650236edf11d5f88ba25854b4c82f97ae0c6
Pedootz <thomas@pedootz.com>
2014-07-06 16:59:31 -0400

    Whoa man behind curtain, whoa there.

--- wp-content/themes/bones/home-bandaid.php
+++ wp-content/themes/bones/home-bandaid.php
@@ -6,21 +6,10 @@ Template Name:Home Band-aid
 
 <?php get_header();
 
-if ( !empty($_GET['pay_attention_to_man_behind_curtain']) ) {
-    $wants_toms_portfolio = strtolower( $_GET['pay_attention_to_man_behind_curtain'] );
-    if ( $wants_toms_portfolio == 'true' ) {
-        $wants_toms_portfolio = true;
-    } else {
-        $wants_toms_portfolio = false;
-    }
-} else {
-    $wants_toms_portfolio = false;
-}
-
-if ( $wants_toms_portfolio ) {
-    get_template_part( 'home' );
-} else {
+if ( array_key_exists('pay_attention_to_man_behind_curtain', $_GET) ) {
     get_template_part( 'home', 'oz' );
+} else {
+    get_template_part( 'home',);
 }
 
 get_footer();
